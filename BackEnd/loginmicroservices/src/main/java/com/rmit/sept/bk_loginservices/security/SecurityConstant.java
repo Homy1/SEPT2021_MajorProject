efaaeee38e47be05317a2ab7a3798f2667f293df
homy1 <amirhomayoon.ashrafzadeh@rmit.edu.au>
2021-08-24 16:29:12 +1000

    slash added for h2

--- BackEnd/loginmicroservices/src/main/java/com/rmit/sept/bk_loginservices/security/SecurityConstant.java
+++ BackEnd/loginmicroservices/src/main/java/com/rmit/sept/bk_loginservices/security/SecurityConstant.java
@@ -3,9 +3,12 @@ package com.rmit.sept.bk_loginservices.security;
 public class SecurityConstant {
 
     public static final String SIGN_UP_URLS = "/api/users/**";
-    public static final String H2_URL = "h2-console/**";
+    public static final String H2_URL = "/h2-console/**";
     public static final String SECRET ="SecretKeyToGenJWTs";
     public static final String TOKEN_PREFIX= "Bearer ";
     public static final String HEADER_STRING = "Authorization";
-    public static final long EXPIRATION_TIME = 30_000; //30 seconds
+    public static final long EXPIRATION_TIME = 30_000;
+    //30 seconds
+    // Slash added
+
 }
